f3396b7867dd36036b807668c027e7cc0c56ebf6
raphaellu <l2lu@ucsd.edu>
2017-05-20 14:17:29 -0700

    filtered ground truth (removed non-existing items)

--- sample_groundtruth.py
+++ sample_groundtruth.py
@@ -9,6 +9,31 @@ def sample_lines(readfile, writefile, sample_rate):
 			f.write(line)
 	f.close()
 
+def check_existing_rate(readfile, writefile):
+	similar_item_item = {}
+	count = 0;
+	total = 0;
+	with open(readfile, 'r') as f:
+		lines = f.readlines()
+		for line in lines:
+			tokens = line.strip().split(',')
+			key = tokens[0]
+			similar_item_item[key] = tokens[1:]
+			total = total + len(tokens[1:])
+	f.close()
+
+	with open(writefile, 'w') as f:
+		for key in similar_item_item:
+			line = key;
+			other_items = similar_item_item[key]
+			for other in other_items:
+				if other in similar_item_item:
+					count = count + 1
+					line = line + ',' + other
+			f.write(line + '\n')
+	f.close()
+	print 'existing rate: ', count/float(total)
+
 if __name__ == '__main__':
 	filename = 'amazon-meta.txt'
 	# 1st arg - readfile path
@@ -17,3 +42,6 @@ if __name__ == '__main__':
 	sample_lines(filename.split('.')[0]+'_ground_truth.txt', 
 		filename.split('.')[0]+'_sampled_ground_truth.txt',
 		0.2)
+
+	check_existing_rate(filename.split('.')[0]+'_ground_truth.txt',
+		filename.split('.')[0]+'_filtered_ground_truth.txt')
